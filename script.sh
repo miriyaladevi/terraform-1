2498c0c8879366c8f54348fa2717c285c017d035
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2023-02-15 17:37:59 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+sudo yum -y install git
+git clone https://github.com/Maddalarajesh/terraform.git
+cd terraform
+terraform init
+terraform validate
+terraform plan
+terraform apply
